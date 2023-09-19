a2630ab284daca0a4761c96c71049ac7e6235949
runner <runner@Mac-1695146822979.local>
2023-09-19 18:10:32 +0000

    horcrust 1.2.1

--- Formula/horcrust.rb
+++ Formula/horcrust.rb
@@ -1,8 +1,8 @@
 class Horcrust < Formula
     desc "Horcrust is a command-line-tool which splits a file into encrypted shards for safekeeping. As long as the specified threshold is met, a user can resurrect their original file at any time - no password necessary."
     homepage "https://github.com/codycline/horcrux-rs"
-    url "https://github.com/codycline/horcrux-rs/releases/download/v1.1.1/horcrust-v1.1.1-apple-darwin.tar.gz"
-    sha256 "d3b52ce68501eb7c295c91bc8257e3396701aff64a4bb7b48d989ad10e660d23"
+    url "https://github.com/codycline/horcrux-rs/releases/download/v1.2.1/horcrust-v1.2.1-apple-darwin.tar.gz"
+    sha256 "871d1aa15e7c1b38e5045547b4ce10d010a4ddc01a4303f2ed303bc8b2e2fab1"
     version ""
   
     def install
